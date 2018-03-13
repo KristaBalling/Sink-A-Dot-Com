c5a7ee50270a61c32080d4a7d7e130b257efb9b0
Krista Blachian <KristaBlachian@outlook.com>
2018-03-13 17:32:28 -0400

    created game object in main

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,6 +3,9 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-
-    }
+    //create the game object
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }//close method
 }
